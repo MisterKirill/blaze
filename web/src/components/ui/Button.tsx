196cf52ba871233c2940626c950092196d02074f
misterkirill <misterkirill666@gmail.com>
2025-02-17 22:05:14 +0500

    feat(web): add router and main layout

---
+++ web/src/components/ui/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes } from "react";
+
+export default function Button({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded cursor-pointer text-sm" {...props}>
+      {children}
+    </button>
+  );
+}
---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import Button from '../components/ui/Button';
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <div className="flex flex-col h-screen bg-zinc-900 text-white">
+      <nav className="flex items-center justify-between px-8 bg-zinc-700 h-16">
+        <Link to="/">
+          <span className="font-bold">Blaze</span>
+        </Link>
+        
+        <Link to="/signup">
+          <Button>Sign Up</Button>
+        </Link>
+      </nav>
+
+      <div className="flex h-full">
+        <div className="md:w-56 bg-zinc-800">sidebar</div>
+
+        <Outlet />
+      </div>
+    </div>
+  )
+}
