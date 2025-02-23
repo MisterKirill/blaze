956da927403cb4003e600b3c56dffb3e4f2bdba9
misterkirill <misterkirill666@gmail.com>
2025-02-24 01:10:14 +0500

    chore: add assets

---
+++ assets/README.md
@@ -0,0 +1,17 @@
+<div align="center">
+    <h1>Blaze Branding</h1>
+</div>
+
+**Big logo:**
+
+big_logo.svg
+
+<img src="./big_logo.svg" height="60">
+
+<br>
+
+**Small logo:**
+
+small_logo.svg
+
+<img src="./small_logo.svg" width="50" height="50">
--- web/src/app/resources/page.tsx
+++ web/src/app/resources/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <h1 className="font-bold text-4xl mb-4">Resources</h1>
       <p className="flex flex-col items-start text-2xl">
         <TextLink href="https://github.com/MisterKirill/blaze">Source code</TextLink>
-        <TextLink href="https://github.com/MisterKirill/blaze/tree/main/web/public">Branding resources</TextLink>
+        <TextLink href="https://github.com/MisterKirill/blaze/tree/main/branding">Branding resources</TextLink>
       </p>
     </>
   );
